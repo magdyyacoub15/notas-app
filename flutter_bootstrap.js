407711f68ad281e03c2c590dabcdf1e1602755da
Magdy <magdy@notas.com>
2026-07-01 16:08:38 +0300

    Shrink balance card height

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "507877335" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3848195907" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
